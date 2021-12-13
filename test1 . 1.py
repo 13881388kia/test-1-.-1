115a3dd475a72398230087892f14d2c08c314a2e
13881388kia <95403139+13881388kia@users.noreply.github.com>
2021-12-13 15:09:01 +0330

    Add files via upload

---
+++ test1 . 1.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+
+
+# if b == 0 :
+#     result = "khata"
+
+# if b != 0 :
+#     result = a / b
+
+
+if b == 0 :
+     result = "khata"
+
+else:
+    result = a / b
+
+print(result)
